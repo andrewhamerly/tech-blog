c62a3236466ef9a7a23b3f8d52caff22aa76fb6d
Andrew Hamerly <73661602+andrewhamerly@users.noreply.github.com>
2024-05-30 14:16:50 -0400

    Created file structure and setup server.js file.

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const session = require('express-session');
+const path = require('path');
+const app = express();
+const sequelize = require('./connection');
+const authRoutes = require('./controllers/authRoutes');
+const postRoutes = require('./controllers/postRoutes');
+const userRoutes = require('./controllers/userRoutes');
+const { User, Post, Comment } = require('./models');
+
+// Middleware for parsing request bodies
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Middleware for static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+}));
+
+sequelize.sync({ alter: true });
+
+app.use('/', authRoutes);
+app.use('/posts', postRoutes);
+app.use('/users', userRoutes);
+
+// Start the server
+const PORT = process.env.PORT || 3001;
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () =>
+    console.log(`Server is running on http://localhost:${PORT}`));
+});
